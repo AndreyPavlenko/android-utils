2d24615b316ccf5a5d28a933d6aeb54a7c6720d2
Andrey Pavlenko <andrey.a.pavlenko@gmail.com>
2020-03-13 21:50:55 +0300

    MiscUtils.isPackageInstalled()

--- src/main/java/me/aap/utils/misc/MiscUtils.java
+++ src/main/java/me/aap/utils/misc/MiscUtils.java
@@ -1,6 +1,7 @@
 package me.aap.utils.misc;
 
-import android.os.Looper;
+import android.content.Context;
+import android.content.pm.PackageManager;
 
 import me.aap.utils.BuildConfig;
 
@@ -8,7 +9,17 @@ import me.aap.utils.BuildConfig;
  * @author Andrey Pavlenko
  */
 public class MiscUtils {
+
 	public static void assertTrue(boolean b) {
 		if (BuildConfig.DEBUG && !b) throw new AssertionError();
+	}
+
+	public static boolean isPackageInstalled(Context ctx, String pkgName) {
+		try {
+			ctx.getPackageManager().getPackageInfo(pkgName, 0);
+			return true;
+		} catch (PackageManager.NameNotFoundException e) {
+			return false;
+		}
 	}
 }
